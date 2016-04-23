956c9c36edb283b2d775e60653bf297222ccd29d
specialshoot <hanzhi911228@163.com>
2016-04-23 18:11:01 +0800

    Initial Commit

---
+++ PictureBoxTest/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace PictureBoxTest {
+    partial class Form1 {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox_Target = new System.Windows.Forms.PictureBox();
+            this.Label_Location = new System.Windows.Forms.Label();
+            this.cbAdd = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Label_Target = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Target)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "打开";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(94, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(646, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(13, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(970, 431);
+            this.panel1.TabIndex = 3;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Controls.Add(this.pictureBox_Target);
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(962, 423);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // pictureBox_Target
+            // 
+            this.pictureBox_Target.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_Target.Image = global::PictureBoxTest.Properties.Resources.favorite_place;
+            this.pictureBox_Target.Location = new System.Drawing.Point(473, 239);
+            this.pictureBox_Target.Name = "pictureBox_Target";
+            this.pictureBox_Target.Size = new System.Drawing.Size(45, 45);
+            this.pictureBox_Target.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_Target.TabIndex = 1;
+            this.pictureBox_Target.TabStop = false;
+            this.pictureBox_Target.Visible = false;
+            // 
+            // Label_Location
+            // 
+            this.Label_Location.AutoSize = true;
+            this.Label_Location.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Label_Location.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_Location.Location = new System.Drawing.Point(225, 478);
+            this.Label_Location.Name = "Label_Location";
+            this.Label_Location.Size = new System.Drawing.Size(100, 23);
+            this.Label_Location.TabIndex = 4;
+            this.Label_Location.Text = "LOCATION";
+            this.Label_Location.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // cbAdd
+            // 
+            this.cbAdd.AutoSize = true;
+            this.cbAdd.Location = new System.Drawing.Point(746, 16);
+            this.cbAdd.Name = "cbAdd";
+            this.cbAdd.Size = new System.Drawing.Size(84, 16);
+            this.cbAdd.TabIndex = 5;
+            this.cbAdd.Text = "设置目标点";
+            this.cbAdd.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(11, 478);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 21);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "坐标：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(9, 510);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(178, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "目标点绝对坐标：";
+            // 
+            // Label_Target
+            // 
+            this.Label_Target.AutoSize = true;
+            this.Label_Target.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Label_Target.Location = new System.Drawing.Point(225, 510);
+            this.Label_Target.Name = "Label_Target";
+            this.Label_Target.Size = new System.Drawing.Size(76, 21);
+            this.Label_Target.TabIndex = 8;
+            this.Label_Target.Text = "TARGET";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(995, 583);
+            this.Controls.Add(this.Label_Target);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbAdd);
+            this.Controls.Add(this.Label_Location);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "图片缩放及图片比例定位";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.pictureBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Target)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Label_Location;
+        private System.Windows.Forms.CheckBox cbAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Label_Target;
+        private System.Windows.Forms.PictureBox pictureBox_Target;
+    }
+}
+
---
+++ PictureBoxTest/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace PictureBoxTest {
+    public partial class Form1 : Form {
+
+        Bitmap myBmp;
+        Point mouseDownPoint = new Point(); //记录拖拽过程鼠标位置
+        bool isMove = false;    //判断鼠标在picturebox上移动时，是否处于拖拽过程(鼠标左键是否按下)
+        int zoomStep = 20;      //缩放步长
+        int oriWidth = 0;
+        int oriHeight = 0;
+        double widthRatio = 1.0;//x方向变换比率
+        double heightRatio = 1.0;//y方向变换比率
+        int targetX = 0;
+        int targetY = 0;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                mouseDownPoint.X = Cursor.Position.X;   //记录鼠标左键按下时位置
+                mouseDownPoint.Y = Cursor.Position.Y;
+                isMove = true;
+                pictureBox1.Focus();    //鼠标滚轮事件(缩放时)需要picturebox有焦点
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                isMove = false;
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e) {
+            pictureBox1.Focus();    //鼠标在picturebox上时才有焦点，此时可以缩放
+            if (isMove) {
+                int x, y;           //新的pictureBox1.Location(x,y)
+                int moveX, moveY;   //X方向，Y方向移动大小。
+                moveX = Cursor.Position.X - mouseDownPoint.X;
+                moveY = Cursor.Position.Y - mouseDownPoint.Y;
+                x = pictureBox1.Location.X + moveX;
+                y = pictureBox1.Location.Y + moveY;
+                pictureBox1.Location = new Point(x, y);
+                mouseDownPoint.X = Cursor.Position.X;
+                mouseDownPoint.Y = Cursor.Position.Y;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            pictureBox1.MouseWheel += new MouseEventHandler(pictureBox1_MouseWheel);
+        }
+
+        /*
+         * pictureBox缩放事件
+         */
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e) {
+            int x = e.Location.X;
+            int y = e.Location.Y;
+            int ow = pictureBox1.Width;
+            int oh = pictureBox1.Height;
+            int VX, VY;     //因缩放产生的位移矢量
+            if (e.Delta > 0)    //放大
+            {
+                //第①步
+                pictureBox1.Width += zoomStep;
+                pictureBox1.Height += zoomStep;
+                //第②步
+                PropertyInfo pInfo = pictureBox1.GetType().GetProperty("ImageRectangle", BindingFlags.Instance |
+                    BindingFlags.NonPublic);
+                Rectangle rect = (Rectangle)pInfo.GetValue(pictureBox1, null);
+                //第③步
+                pictureBox1.Width = rect.Width;
+                pictureBox1.Height = rect.Height;
+            }
+            if (e.Delta < 0)    //缩小
+            {
+                //防止一直缩成负值
+                if (pictureBox1.Width < myBmp.Width / 10)
+                    return;
+
+                pictureBox1.Width -= zoomStep;
+                pictureBox1.Height -= zoomStep;
+                PropertyInfo pInfo = pictureBox1.GetType().GetProperty("ImageRectangle", BindingFlags.Instance |
+                    BindingFlags.NonPublic);
+                Rectangle rect = (Rectangle)pInfo.GetValue(pictureBox1, null);
+                pictureBox1.Width = rect.Width;
+                pictureBox1.Height = rect.Height;
+            }
+            //第④步，求因缩放产生的位移，进行补偿，实现锚点缩放的效果
+            VX = (int)((double)x * (ow - pictureBox1.Width) / ow);
+            VY = (int)((double)y * (oh - pictureBox1.Height) / oh);
+            pictureBox1.Location = new Point(pictureBox1.Location.X + VX, pictureBox1.Location.Y + VY);
+            int tarX = (int)(targetX * (pictureBox1.Width * 1.0 / oriWidth));
+            int tarY = (int)(targetY * (pictureBox1.Height * 1.0 / oriHeight));
+            pictureBox_Target.Location = new Point(tarX - pictureBox_Target.Width / 2, tarY - pictureBox_Target.Height);
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            string filename = "";
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Tiff文件|*.tif|Bmp文件|*.bmp|Erdas img文件|*.img|EVNI文件|*.hdr|jpeg文件|*.jpg|raw文件|*.raw|vrt文件|*.vrt|所有文件|*.*";
+            dlg.FilterIndex = 8;
+            if (dlg.ShowDialog() == DialogResult.OK) {
+                filename = dlg.FileName;
+            }
+            if (filename == "") {
+                return;
+            }
+            myBmp = new Bitmap(filename);
+            if (myBmp == null) {
+                MessageBox.Show("读取失败");
+                return;
+            }
+            textBox1.Text = filename;
+            pictureBox1.Image = myBmp;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; //设置picturebox为缩放模式
+            pictureBox1.Width = myBmp.Width;
+            pictureBox1.Height = myBmp.Height;
+            oriWidth = myBmp.Width;
+            oriHeight = myBmp.Height;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                mouseDownPoint.X = Cursor.Position.X;
+                mouseDownPoint.Y = Cursor.Position.Y;
+                isMove = true;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e) {
+            if (e.Button == MouseButtons.Left) {
+                isMove = false;
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e) {
+            panel1.Focus();
+            if (isMove) {
+                int x, y;
+                int moveX, moveY;
+                moveX = Cursor.Position.X - mouseDownPoint.X;
+                moveY = Cursor.Position.Y - mouseDownPoint.Y;
+                x = pictureBox1.Location.X + moveX;
+                y = pictureBox1.Location.Y + moveY;
+                pictureBox1.Location = new Point(x, y);
+                mouseDownPoint.X = Cursor.Position.X;
+                mouseDownPoint.Y = Cursor.Position.Y;
+                int tarX = (int)(targetX * (pictureBox1.Width * 1.0 / oriWidth));
+                int tarY = (int)(targetY * (pictureBox1.Height * 1.0 / oriHeight));
+                pictureBox_Target.Location = new Point(tarX - pictureBox_Target.Width / 2, tarY - pictureBox_Target.Height);
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e) {
+            widthRatio = pictureBox1.Width * 1.0 / oriWidth;
+            heightRatio = pictureBox1.Height * 1.0 / oriHeight;
+            int afterSizeX = e.X * oriWidth / pictureBox1.Width;
+            int afterSizeY = e.Y * oriHeight / pictureBox1.Height;
+            Label_Location.Text = e.X + ";" + e.Y + ";" + widthRatio + ";" + heightRatio + ";" + afterSizeX + ";" + afterSizeY;
+            if (cbAdd.Checked) {
+                pictureBox_Target.Visible = Visible;
+                pictureBox_Target.Location = new Point(e.X - pictureBox_Target.Width / 2, e.Y - pictureBox_Target.Height);
+                targetX = afterSizeX;
+                targetY = afterSizeY;
+            }
+        }
+    }
+}
